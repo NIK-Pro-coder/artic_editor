2f44fe6465e55f32071918802fe77a6e2d1f097e
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2024-04-25 11:45:05 +0200

    Started implementing a function to read a .tic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
+use std::{fs::File, io::{self, Read, Seek, SeekFrom}};
+
+struct chunk {
+    r#type : u32,
+    size : u32,
+    bank : u8,
+    data : Vec<u8>,
+}
+
+fn read_chunk(data: Vec<u8>, offset: u32) -> () {}
+
+fn read_tic(data: Vec<u8> ) -> () {
+    u32 skip = 0;
+}
+
+fn main() -> () {
     println!("Hello, world!");
+
+    let start = 0;
+    let count = 4;
+
+    let mut f = File::open("test.tic").expect("No file found!");
+    f.seek(SeekFrom::Start(start));
+    let mut buf = vec![0; count];
+    f.read_exact(&mut buf);
+    read_tic(buf)
 }
